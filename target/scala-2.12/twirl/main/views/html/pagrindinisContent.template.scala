ee1e383ce3ce5fef309a61095e6c6c5ba390e028
Aleksandr Polescuk <a.polescuk@gmail.com>
2017-07-16 00:32:26 +0200

    Contact form added

--- target/scala-2.12/twirl/main/views/html/pagrindinisContent.template.scala
+++ target/scala-2.12/twirl/main/views/html/pagrindinisContent.template.scala
@@ -97,7 +97,7 @@ Seq[Any](format.raw/*1.43*/("""
 <!-- About Section -->
 <section id="about" class="container content-section text-center">
     <div class="row">
-        <div class="col-lg-4" >
+        <div class="col-lg-4">
 
             <h2 align="center"><u>Išsilavinimas:</u></h2>
             <ul>
@@ -114,13 +114,13 @@ Seq[Any](format.raw/*1.43*/("""
 
             <h2 align="center"><u>Kursai:</u></h2>
             <ul>
-                <li >2010 metai - "Baltic SPA Professional" Kursai (Latvija), SPA masažai.</li>
-                <li>2012 metai - Slaugos darbuotojų tobulinimosi ir specializacijos centras. </li>
+                <li>2010 metai - "Baltic SPA Professional" Kursai (Latvija), SPA masažai.</li>
+                <li>2012 metai - Slaugos darbuotojų tobulinimosi ir specializacijos centras.</li>
                 <li>Pažymėjimas Nr. KV 120146 - suteikta teisė daryti gydomuosius masažus.</li>
             </ul>
         </div>
         <div class="col-lg-4 ">
-            <img src='"""),_display_(/*91.24*/routes/*91.30*/.Assets.at("images/img/as2.jpg")),format.raw/*91.62*/("""' class="img-circle" alt="Medicine massage" height="600" >
+            <img src='"""),_display_(/*91.24*/routes/*91.30*/.Assets.at("images/img/as2.jpg")),format.raw/*91.62*/("""' class="img-circle" alt="Medicine massage" height="600">
 
         </div>
         <div class="col-lg-4 ">
@@ -129,14 +129,14 @@ Seq[Any](format.raw/*1.43*/("""
             <ul>
 
                 <li>Klasikinis segmentinis masažas</li>
-                <li>Karštų-šaltų akmenų masažas </li>
+                <li>Karštų-šaltų akmenų masažas</li>
                 <li>Limfodrenažinis masažas</li>
                 <li>Thai pėdų masažas</li>
-                <li>Tradicinis ajurvedinis masažas </li>
+                <li>Tradicinis ajurvedinis masažas</li>
             </ul>
-            <h2 align="center"><u>Gydomieji masažai:</u> </h2>
+            <h2 align="center"><u>Gydomieji masažai:</u></h2>
             <ul>
-            <li>Sergant sąnarių, stuburo, nervų, širdies-kraujagyslių, kvėpavimo sistemų ligomis</li>
+                <li>Sergant sąnarių, stuburo, nervų, širdies-kraujagyslių, kvėpavimo sistemų ligomis</li>
                 <li>Medžiagų apykaitos sutrikimo atvejais</li>
                 <li>Segmentinis masažas</li>
                 <li>Jungiamojo audinio masažas</li>
@@ -158,42 +158,71 @@ Seq[Any](format.raw/*1.43*/("""
                 <div class="col-lg-4 ">
 
 
-            <button type="button" class="btn btn-info" data-toggle="collapse" data-target="#demo">Limfodrenažinis masažas</button>
-            <div id="demo" class="collapse">
-                <div class="text-center"> <img src='"""),_display_(/*131.54*/routes/*131.60*/.Assets.at("images/img/massage164.jpg")),format.raw/*131.99*/("""' class="img-circle" alt="Medicine massage" width="250" height="250"></a></div>
-                <div id="lopas">
-                <p class="text-justify">
-                <ul>
-                  <li>Tai saugus efektyvus būdas sustiprinti sveikatą bei palaikyti gerą savijautą. Sveikiems žmonėms - tai profilaktinė priemonė, palaikanti normalią medžiagų apykaitą.</li>
-                <li> Masažo metu suaktyvinami pažeistų minkštųjų audinių regeneracijos procesai,  slopinami uždegiminiai procesai, sustiprinamas imunitetas, pagerinamas audinių aprūpinimas deguonimi.</li>
-                <li>  Limfodrenažinis masažas tinka visiems, kurie jaučia kojų pabrinkimą, patinimą ar tiesiog nuovargį. Po ilgos dienos jis puikiai atpalaiduoja pavargusias kojas ir suteikia lengvumo pojūtį. Taip pat tinka žmonėms, kurių kojos visada šaltos.</li>
-                <li> Šis masažas grožio tikslais naudojamas celiulito gydymui ir profilaktikai, odos stangrinimui, apimčių mažinimui, bendrai savijautai gerinti ir mažinti patinimus  po plastinių operacijų.</li>
-                <li>  Šis masažas atliekamas moterims ir vyrams.
-                <li>  Gydymui ši procedūra skiriama kasdien, profilaktiškai - 2-3 kartus per savaitę. Norimas efektas pasiekiamas po 15 procedūrų.</li></p>
+                    <button type="button" class="btn btn-info" data-toggle="collapse" data-target="#demo">
+                        Limfodrenažinis masažas
+                    </button>
+                    <div id="demo" class="collapse">
+                        <div class="text-center"><img src='"""),_display_(/*133.61*/routes/*133.67*/.Assets.at("images/img/massage164.jpg")),format.raw/*133.106*/("""'
+                                                      class="img-circle" alt="Medicine massage" width="250"
+                                                      height="250"></a></div>
+                        <div id="lopas">
+                            <p class="text-justify">
+                            <ul>
+                                <li>Tai saugus efektyvus būdas sustiprinti sveikatą bei palaikyti gerą savijautą.
+                                    Sveikiems žmonėms - tai profilaktinė priemonė, palaikanti normalią medžiagų
+                                    apykaitą.
+                                </li>
+                                <li> Masažo metu suaktyvinami pažeistų minkštųjų audinių regeneracijos procesai,
+                                    slopinami uždegiminiai procesai, sustiprinamas imunitetas, pagerinamas audinių
+                                    aprūpinimas deguonimi.
+                                </li>
+                                <li> Limfodrenažinis masažas tinka visiems, kurie jaučia kojų pabrinkimą, patinimą ar
+                                    tiesiog nuovargį. Po ilgos dienos jis puikiai atpalaiduoja pavargusias kojas ir
+                                    suteikia lengvumo pojūtį. Taip pat tinka žmonėms, kurių kojos visada šaltos.
+                                </li>
+                                <li> Šis masažas grožio tikslais naudojamas celiulito gydymui ir profilaktikai, odos
+                                    stangrinimui, apimčių mažinimui, bendrai savijautai gerinti ir mažinti patinimus po
+                                    plastinių operacijų.
+                                </li>
+                                <li> Šis masažas atliekamas moterims ir vyrams.
+                                <li> Gydymui ši procedūra skiriama kasdien, profilaktiškai - 2-3 kartus per savaitę.
+                                    Norimas efektas pasiekiamas po 15 procedūrų.
+                                </li>
+                            </p>
 
-                    </ul></div></div>
-            </div>
+                            </ul></div>
+                    </div>
+                </div>
 
                 <div class="col-lg-4 ">
 
 
-                    <button type="button" class="btn btn-info" data-toggle="collapse" data-target="#demo2">Karštų-šaltų akmenų masažas</button>
+                    <button type="button" class="btn btn-info" data-toggle="collapse" data-target="#demo2">Karštų-šaltų
+                        akmenų masažas
+                    </button>
                     <div id="demo2" class="collapse">
-                        <div class="text-center"> <img src='"""),_display_(/*150.62*/routes/*150.68*/.Assets.at("images/img/mas3.jpeg")),format.raw/*150.102*/("""' class="img-circle" alt="Medicine massage" width="250" height="250"></a></div>
+                        <div class="text-center"><img src='"""),_display_(/*172.61*/routes/*172.67*/.Assets.at("images/img/mas3.jpeg")),format.raw/*172.101*/("""' class="img-circle"
+                                                      alt="Medicine massage" width="250" height="250"></a></div>
                         <div id="lopas">
                             <p class="text-justify">
-                        <ul>
-                        <li>     Akmenys turi savybę iš raumenų pašalinti toksines medžiagas, susikaupusias dėl streso ir nesveiko gyvenimo būdo, gerina miegą, padeda  sureguliuoti  kraujospūdį, teigiamai   veikia   širdies  ritmą,  sužadinamos natūralios organizmo galios, padedančios įveikti ligas, paspartinamas gijimo procesas.
-                            Ši terapija veikia daugelį organizmo grandžių:</li>
-                        <li> Stimuliuoja imuninę sistemą;</li>
-                        <li> Suaktyvina kraujotaką;</li>
-                        <li>Pagerėja aprūpinimas deguonimi;</li>
-                        <li> Lengvai ir maloniai numalšina raumenų, nugaros, galvos ir kaklo skausmus;</li>
-                        <li> Teigiamai veikia nervinę sistemą;</li>
-                        <li>Gerindama metabolizmą, padeda atsikratyti riebalų pertekliaus.</li>
-                        <li>Šis   masažas  ypač   tinka pervargus, patyrus gilų stresą,  jaučiant  nervingumą,  apatiją,  miego sutrikimus.</li>
+                            <ul>
+                                <li> Akmenys turi savybę iš raumenų pašalinti toksines medžiagas, susikaupusias dėl
+                                    streso ir nesveiko gyvenimo būdo, gerina miegą, padeda sureguliuoti kraujospūdį,
+                                    teigiamai veikia širdies ritmą, sužadinamos natūralios organizmo galios, padedančios
+                                    įveikti ligas, paspartinamas gijimo procesas.
+                                    Ši terapija veikia daugelį organizmo grandžių:
+                                </li>
+                                <li> Stimuliuoja imuninę sistemą;</li>
+                                <li> Suaktyvina kraujotaką;</li>
+                                <li>Pagerėja aprūpinimas deguonimi;</li>
+                                <li> Lengvai ir maloniai numalšina raumenų, nugaros, galvos ir kaklo skausmus;</li>
+                                <li> Teigiamai veikia nervinę sistemą;</li>
+                                <li>Gerindama metabolizmą, padeda atsikratyti riebalų pertekliaus.</li>
+                                <li>Šis masažas ypač tinka pervargus, patyrus gilų stresą, jaučiant nervingumą, apatiją,
+                                    miego sutrikimus.
+                                </li>
 
-                        </ul>
+                            </ul>
                         </div>
                     </div>
                 </div>
@@ -201,43 +230,69 @@ Seq[Any](format.raw/*1.43*/("""
                 <div class="col-lg-4 ">
 
 
-                    <button type="button" class="btn btn-info" data-toggle="collapse" data-target="#demo3">Klasikinis Segmentinis masažas</button>
+                    <button type="button" class="btn btn-info" data-toggle="collapse" data-target="#demo3">Klasikinis
+                        Segmentinis masažas
+                    </button>
                     <div id="demo3" class="collapse">
-                        <div class="text-center"> <img src='"""),_display_(/*174.62*/routes/*174.68*/.Assets.at("images/img/mas2.jpg")),format.raw/*174.101*/("""' class="img-circle" alt="Medicine massage" width="250" height="250"></a></div>
+                        <div class="text-center"><img src='"""),_display_(/*205.61*/routes/*205.67*/.Assets.at("images/img/mas2.jpg")),format.raw/*205.100*/("""' class="img-circle"
+                                                      alt="Medicine massage" width="250" height="250"></a></div>
                         <div id="lopas">
-                        <p class="text-justify">
-                            <ul><li>Klasikinis lalallalalalalallalalala ir t.t. </li></ul>
+                            <p class="text-justify">
+                            <ul>
+                                <li>Klasikinis lalallalalalalallalalala ir t.t.</li>
+                            </ul>
 
-                    </div></div>
+                        </div>
+                    </div>
                 </div>
 
 
-
-                </div>
             </div>
         </div>
+    </div>
 
 </section>
 
 <!-- Contact Section -->
+
+
 <section id="contact" class="container content-section text-center">
     <div class="row">
-        <div class="col-lg-8 col-lg-offset-2">
-            <h2>Contact Start Bootstrap</h2></p>
-            <p>Feel free to email us to provide some feedback on our templates, give us suggestions for new templates and themes, or to just say hello!</p>
+        <div class="col-lg-12">
+            <div class="contact-section">
+                <div class="container">
+                    <h2>Užduokite savo klausimą!</h2>
+                    <p></p>
+                    <div class="row">
+                        <div class="col-md-8 col-md-offset-2">
+                            <form class="form-horizontal">
+                                <div class="form-group">
+                                    <label for="exampleInputName2">Vardas</label>
+                                    <input type="text" class="form-control" id="exampleInputName2" placeholder="Stasys Bardiuras" required="required">
+                                </div>
+                                <div class="form-group">
+                                    <label for="exampleInputEmail2">Elektroninis paštas</label>
+                                    <input type="email" class="form-control" id="exampleInputEmail2" placeholder="Jane'eta'vania.lt" required="required">
+                                </div>
+                                <div class="form-group">
+                                    <label for="exampleInputText">Jūsų žinutė</label>
+                                    <textarea  class="form-control" id="exampleInputText" placeholder="Klausimas" required="required"></textarea>
+                                </div>
+                                <button type="submit" class="btn btn-default">Siųsti žinute</button>
+                            </form>
 
-            </p>
-            <ul class="list-inline banner-social-buttons">
-                <li>
-                    <a href="https://twitter.com/SBootstrap" class="btn btn-default btn-lg"><i class="fa fa-twitter fa-fw"></i> <span class="network-name">Twitter</span></a>
-                </li>
-                <li>
-                    <a href="https://github.com/IronSummitMedia/startbootstrap" class="btn btn-default btn-lg"><i class="fa fa-github fa-fw"></i> <span class="network-name">Github</span></a>
-                </li>
-                <li>
-                    <a href="https://plus.google.com/+Startbootstrap/posts" class="btn btn-default btn-lg"><i class="fa fa-google-plus fa-fw"></i> <span class="network-name">Google+</span></a>
-                </li>
-            </ul>
+                            <hr>
+                            <h3>Mūsų socialiniai tinklai</h3>
+                            <ul class="list-inline banner-social-buttons">
+                                <li><a href="#" class="btn btn-default btn-lg"><i class="fa fa-twitter"> <span class="network-name">Twitter</span></i></a></li>
+                                <li><a href="#" class="btn btn-default btn-lg"><i class="fa fa-facebook"> <span class="network-name">Facebook</span></i></a></li>
+                                <li><a href="#" class="btn btn-default btn-lg"><i class="fa fa-youtube-play"> <span class="network-name">Youtube</span></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+
         </div>
     </div>
 </section>
@@ -253,22 +308,22 @@ Seq[Any](format.raw/*1.43*/("""
 </footer>
 
 <!-- jQuery -->
-<script src='"""),_display_(/*224.15*/routes/*224.21*/.Assets.at("stylesheets/vendor/jquery/jquery.js")),format.raw/*224.70*/("""'></script>
+<script src='"""),_display_(/*279.15*/routes/*279.21*/.Assets.at("stylesheets/vendor/jquery/jquery.js")),format.raw/*279.70*/("""'></script>
 
 <!-- Bootstrap Core JavaScript -->
-<script src='"""),_display_(/*227.15*/routes/*227.21*/.Assets.at("stylesheets/vendor/bootstrap/js/bootstrap.min.js")),format.raw/*227.83*/("""'></script>
+<script src='"""),_display_(/*282.15*/routes/*282.21*/.Assets.at("stylesheets/vendor/bootstrap/js/bootstrap.min.js")),format.raw/*282.83*/("""'></script>
 
 <!-- Plugin JavaScript -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
 
 <!-- Google Maps API Key - Use your own API key to enable the map feature. More information on the Google Maps API can be found at https://developers.google.com/maps/ -->
-<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCRngKslUGJTlibkQ3FkfTxj3Xss1UlZDA&sensor=false"></script>
+<script type="text/javascript"
+        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBtkVXlKJlSvm85a7oRDMLw4ZOZaxYwKxM&sensor=false"></script>
 
 <!-- Theme JavaScript -->
-<script src='"""),_display_(/*236.15*/routes/*236.21*/.Assets.at("javascripts/js/grayscale.min.js")),format.raw/*236.66*/("""'></script>
+<script src='"""),_display_(/*292.15*/routes/*292.21*/.Assets.at("javascripts/js/grayscale.min.js")),format.raw/*292.66*/("""'></script>
 
 </body>
-
 
 
 """)))}))
@@ -287,11 +342,11 @@ Seq[Any](format.raw/*1.43*/("""
 
               /*
                   -- GENERATED --
-                  DATE: Sat Jul 15 23:58:40 CEST 2017
+                  DATE: Sun Jul 16 00:31:24 CEST 2017
                   SOURCE: /home/alex/git/web/app/views/pagrindinisContent.scala.html
-                  HASH: 567b62c2732b1ed7a8f5bd1a327c44e2783eb504
-                  MATRIX: 968->1|1104->42|1132->45|1179->84|1229->97|1257->99|4707->3522|4722->3528|4775->3560|6227->4984|6243->4990|6304->5029|7961->6658|7977->6664|8034->6698|9696->8332|9712->8338|9768->8371|11585->10160|11601->10166|11672->10215|11762->10277|11778->10283|11862->10345|12389->10844|12405->10850|12472->10895
-                  LINES: 28->1|33->1|35->3|35->3|35->3|37->5|123->91|123->91|123->91|163->131|163->131|163->131|182->150|182->150|182->150|206->174|206->174|206->174|256->224|256->224|256->224|259->227|259->227|259->227|268->236|268->236|268->236
+                  HASH: 2dacbc4c85b4c5297c21a737f0564cdc8a153cea
+                  MATRIX: 968->1|1104->42|1132->45|1179->84|1229->97|1257->99|4704->3519|4719->3525|4772->3557|6293->5050|6309->5056|6371->5095|8852->7548|8868->7554|8925->7588|10954->9589|10970->9595|11026->9628|14213->12787|14229->12793|14300->12842|14390->12904|14406->12910|14490->12972|15025->13479|15041->13485|15108->13530
+                  LINES: 28->1|33->1|35->3|35->3|35->3|37->5|123->91|123->91|123->91|165->133|165->133|165->133|204->172|204->172|204->172|237->205|237->205|237->205|311->279|311->279|311->279|314->282|314->282|314->282|324->292|324->292|324->292
                   -- GENERATED --
               */
           
