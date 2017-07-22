99bb83a09ad91e13b778ccfa6797401a064f0220
Aleksandr Polescuk <a.polescuk@gmail.com>
2017-07-22 20:27:10 +0200

    Revert "Some info added"
    This reverts commit 418f78af82f9e7ecf8d89fa8bbc4e0320423e081.

--- app/Module.java
+++ app/Module.java
@@ -10,12 +10,12 @@ import services.Counter;
  * different types. This Guice module is created when the Play
  * application starts.
  *
- * Play will automatically use any class called `Module2` that is in
+ * Play will automatically use any class called `Module` that is in
  * the root package. You can create modules in other locations by
  * adding `play.modules.enabled` settings to the `application.conf`
  * configuration file.
  */
-public class Module2 extends AbstractModule {
+public class Module extends AbstractModule {
 
     @Override
     public void configure() {
