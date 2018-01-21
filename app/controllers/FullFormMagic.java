e4fa1adfc645880a3e9f5ea5b4333067fbeafd71
Aleksandr Polescuk <a.polescuk@gmail.com>
2018-01-21 13:01:02 +0100

    revert this if f'uped

--- app/controllers/FullFormMagic.java
+++ app/controllers/FullFormMagic.java
@@ -7,6 +7,8 @@ import play.libs.Json;
 import play.mvc.Result;
 
 import java.io.UnsupportedEncodingException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -31,10 +33,20 @@ public class FullFormMagic {
     private static final String TIME = "time";
     private static final String MESSAGE = "message";
 
+    private static Connection getConnection() throws URISyntaxException, SQLException {
+        URI dbUri = new URI(System.getenv("CLEARDB_DATABASE_URL"));
 
-    public Result addBookingToDatabase() throws UnsupportedEncodingException {
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:mysql://" + dbUri.getHost() + dbUri.getPath();
+
+        return (Connection) DriverManager.getConnection(dbUrl, username, password);
+    }
+
+    public Result addBookingToDatabase() throws UnsupportedEncodingException, URISyntaxException {
         JsonNode json = request().body().asJson();
-        try (com.mysql.jdbc.Connection connection = (com.mysql.jdbc.Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/play", "root", "root")) {
+//        try (com.mysql.jdbc.Connection connection = (com.mysql.jdbc.Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/play", "root", "root")) {
+        try (com.mysql.jdbc.Connection connection = getConnection()) {
 
             PreparedStatement xs = connection.prepareStatement("INSERT INTO play.fullReservationForm (name, surname, email, phone, massage, date, time, message)  VALUES ( ?, ?, ?, ?, ?, ?, ?, ?)");
             xs.setString(1, setMaxColTypeLen(findJson(json, NAME), getMaxSize(connection, NAME)));
